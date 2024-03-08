4835be89761a1817eb2a4a91f1796ed309310b34
TGSXVII <madsduly@gmail.com>
2024-03-08 10:01:31 +0100

    Added styling, Scripts, place holder picture and homepage

---
+++ JavaScript/sideMenu.js
@@ -0,0 +1,7 @@
+function openmenu() {
+    document.getElementById('sidemenu').style.right = '0';
+}
+
+function closemenu() {
+    document.getElementById('sidemenu').style.right = '-200px'; // or whatever the original position is
+}
